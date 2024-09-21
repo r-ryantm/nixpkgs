ef0ce32f4f3eb90779cdb52eb41e011d84a7ace4
natsukium <tomoya.otabi@gmail.com>
2024-09-21 15:17:46 +0900

    python312Packages.confluent-kafka: build with pypaBuildHook

--- pkgs/development/python-modules/confluent-kafka/default.nix
+++ pkgs/development/python-modules/confluent-kafka/default.nix
@@ -13,12 +13,13 @@
   rdkafka,
   requests,
   requests-mock,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "confluent-kafka";
   version = "2.5.3";
-  format = "setuptools";
+  pyproject = true;
 
   disabled = pythonOlder "3.7";
 
@@ -31,25 +32,31 @@ buildPythonPackage rec {
 
   buildInputs = [ rdkafka ];
 
-  propagatedBuildInputs = [ requests ];
+  build-system = [ setuptools ];
 
-  passthru.optional-dependencies = {
+  optional-dependencies = {
     avro = [
       avro
       fastavro
+      requests
     ];
     json = [
       jsonschema
       pyrsistent
+      requests
     ];
-    protobuf = [ protobuf ];
+    protobuf = [
+      protobuf
+      requests
+    ];
+    schema-registry = [ requests ];
   };
 
   nativeCheckInputs = [
     pyflakes
     pytestCheckHook
     requests-mock
-  ] ++ lib.flatten (builtins.attrValues passthru.optional-dependencies);
+  ] ++ lib.flatten (builtins.attrValues optional-dependencies);
 
   pythonImportsCheck = [ "confluent_kafka" ];
 
